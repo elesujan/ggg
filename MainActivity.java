4019b0b3ed51eb54ec1370bfe48fc5ab130316b7
elesujan <43334833+elesujan@users.noreply.github.com>
2018-09-17 14:24:53 +0545

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.welloworld.department_sept12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+//    Teacher teacher1,teacher2;
+//    HOD hodelx, hodele;
+//    ProjectCordinator elexc, electc;
+//
+//    final static String TAG = "TAG_MAIN";
+//    String arrystringn={"1","two","hello"};
+//    List<String> arraystringNaya = new ArrayList<>(); //define array
+//
+//    PrivatePublictest test1, test2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+//        teacher1 = new Teacher();
+//        teacher2 = new Teacher();
+//        hodele = new HOD();
+//        hodelx = new HOD();
+//        elexc = new ProjectCordinator();
+//        electc = new ProjectCordinator();
+//        hodele.name = "Narayan prasad Acharya";
+//        hodele.name = "Rupesh D Sherestha";
+//        teacher1.name = "Sujan Acharya";
+//        teacher1.age = 25;
+//        teacher1.faculty = "Electrical";
+//        teacher1.sex = "Male";
+//
+//        arraystringNaya.add("1");
+//        arraystringNaya.add("two");
+//        arraystringNaya.add("hello");
+//        for (int i=0;i<arraystring.length;i++){
+//            Log.d(TAG,arraystring[i]);
+//            for (String str:arraystringNaya){//initation of string
+//                Log.d(TAG,str);
+//            }
+//        }
+//
+//        Log.d (TAG,"" + teacher1.age);
+//        Log.d (TAG,  teacher1.name);
+//        teacher1.teaching();
+//        Teacher.college = "Nepal Engineering College";
+//        Log.d(TAG, "hello world ");
+//        int areasquare = teacher1.FindArea(5);
+//        int areaRect = teacher1.FindArea(5, 7);
+//        Log.d(TAG,"Area of square is"+ areasquare);
+//        Log.d(TAG,"area of rectangale is "+ areaRect);
+//       teacher1.salary();
+//       hodelx.salary();
+//       electc.salary();
+//       elexc.salary();
+//       test1= new PrivatePublictest();
+//       test2= new PrivatePublictest();
+//       test1.test1 = "J rakhe ni hunxa";
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
